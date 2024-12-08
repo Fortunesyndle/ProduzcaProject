908def21598ceee078758539eb9108b15d94d48c
User <User@DESKTOP-HE038JM>
2024-12-08 14:25:06 -0400

    Merge origin/master
    Conflicts:
    	src/raven/form/Prestamos.form
    	src/raven/form/Prestamos.java

--- src/raven/form/Dashboard.java
+++ src/raven/form/Dashboard.java
@@ -1,6 +1,8 @@
 package raven.form;
 
 import com.formdev.flatlaf.FlatClientProperties;
+import com.raven.model.Model_Card;
+import java.awt.Color;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,6 +17,9 @@ import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javaswingdev.chart.ModelPieChart;
+import javaswingdev.chart.PieChart;
+import javax.swing.ImageIcon;
 import raven.conexion.Conexion;
 import raven.swing.AvatarIcon;
 
@@ -29,14 +34,32 @@ public class Dashboard extends javax.swing.JPanel {
         initComponents();
         lb.putClientProperty(FlatClientProperties.STYLE, ""
                 + "font:$h1.font");
-        jPanel1.putClientProperty(FlatClientProperties.STYLE, ""
-                + "arc:50;"
-                + "[light]background:darken(@background,6%);"
-                + "[dark]background:lighten(@background,6%)");
-       
+        card1.setData(new Model_Card(new ImageIcon(getClass().getResource("/com/raven/icon/icons8-añadir-50 (1).png")), "Agregar", "", "Puedes agregar aquí"));
+        card2.setData(new Model_Card(new ImageIcon(getClass().getResource("/com/raven/icon/icons8-editar-archivo-50.png")), "Editar", "", "Puedes editar aquí"));
+        card3.setData(new Model_Card(new ImageIcon(getClass().getResource("/com/raven/icon/icons8-eliminar-50.png")), "Eliminar", "", "Puedes eliminar aquí"));        
         
-        avatar.setIcon(new AvatarIcon(getClass().getResource("/raven/image/astrid.jpg"), 150, 150, 1000));
-       
+        
+        
+        pieChart1.setChartType(PieChart.PeiChartType.DONUT_CHART);
+        pieChart1.addData(new ModelPieChart("Tigher", 150, new Color(23, 126, 238)));
+        pieChart1.addData(new ModelPieChart("ABC", 100, new Color(221, 65, 65)));
+        pieChart1.addData(new ModelPieChart("Coca", 1, new Color(47, 157, 64)));
+        pieChart1.addData(new ModelPieChart("Vita", 60, new Color(196, 151, 58)));
+        
+        
+        pieChart2.setChartType(PieChart.PeiChartType.DONUT_CHART);
+        pieChart2.addData(new ModelPieChart("Tigher", 150, new Color(23, 126, 238)));
+        pieChart2.addData(new ModelPieChart("ABC", 100, new Color(221, 65, 65)));
+        pieChart2.addData(new ModelPieChart("Coca", 1, new Color(47, 157, 64)));
+        pieChart2.addData(new ModelPieChart("Vita", 60, new Color(196, 151, 58)));
+        
+        
+        pieChart3.setChartType(PieChart.PeiChartType.DONUT_CHART);
+        pieChart3.addData(new ModelPieChart("Tigher", 150, new Color(23, 126, 238)));
+        pieChart3.addData(new ModelPieChart("ABC", 100, new Color(221, 65, 65)));
+        pieChart3.addData(new ModelPieChart("Coca", 1, new Color(47, 157, 64)));
+        pieChart3.addData(new ModelPieChart("Vita", 60, new Color(196, 151, 58)));
+        
         mostrarMensajeAleatorio();
     }
     
@@ -89,28 +112,157 @@ public class Dashboard extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        modelPieChart1 = new javaswingdev.chart.ModelPieChart();
         lb = new javax.swing.JLabel();
+        labelMensaje = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        card1 = new com.raven.component.Card();
+        card2 = new com.raven.component.Card();
+        card3 = new com.raven.component.Card();
+        pieChart1 = new javaswingdev.chart.PieChart();
+        pieChart2 = new javaswingdev.chart.PieChart();
+        pieChart3 = new javaswingdev.chart.PieChart();
 
         lb.setText("Bienvenida de nuevo, Astrid Hernández");
+
+        labelMensaje.setText("jLabel1");
+
+        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel3MouseClicked(evt);
+            }
+        });
+
+        jLabel4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel4MouseClicked(evt);
+            }
+        });
+
+        jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel5MouseClicked(evt);
+            }
+        });
+
+        card1.setColor1(new java.awt.Color(142, 142, 250));
+        card1.setColor2(new java.awt.Color(123, 123, 245));
+
+        card2.setColor1(new java.awt.Color(186, 123, 247));
+        card2.setColor2(new java.awt.Color(167, 94, 236));
+        card2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                card2MouseClicked(evt);
+            }
+        });
+
+        card3.setColor1(new java.awt.Color(241, 208, 62));
+        card3.setColor2(new java.awt.Color(211, 184, 61));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(460, 460, 460)
-                .addComponent(lb))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(7, 7, 7)
+                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(card1, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(10, 10, 10)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(card2, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(7, 7, 7)
+                                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(10, 10, 10)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(card3, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(9, 9, 9)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 265, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(361, 361, 361)
+                        .addComponent(lb))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(555, 555, 555)
+                        .addComponent(labelMensaje)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(111, 111, 111)
+                .addComponent(pieChart1, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(196, 196, 196)
+                .addComponent(pieChart2, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 196, Short.MAX_VALUE)
+                .addComponent(pieChart3, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(92, 92, 92))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(80, 80, 80)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(32, 32, 32)
                 .addComponent(lb)
-                .addGap(545, 545, 545))
+                .addGap(35, 35, 35)
+                .addComponent(labelMensaje)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(card1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(card2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(5, 5, 5)
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(card3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 102, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(pieChart3, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pieChart1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pieChart2, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(79, 79, 79))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jLabel3MouseClicked
+
+    private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel4MouseClicked
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_jLabel4MouseClicked
+
+    private void jLabel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseClicked
+        // TODO add your handling code here:
+     
+    }//GEN-LAST:event_jLabel5MouseClicked
+
+    private void card2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_card2MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_card2MouseClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.raven.component.Card card1;
+    private com.raven.component.Card card2;
+    private com.raven.component.Card card3;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel labelMensaje;
     private javax.swing.JLabel lb;
+    private javaswingdev.chart.ModelPieChart modelPieChart1;
+    private javaswingdev.chart.PieChart pieChart1;
+    private javaswingdev.chart.PieChart pieChart2;
+    private javaswingdev.chart.PieChart pieChart3;
     // End of variables declaration//GEN-END:variables
 }
